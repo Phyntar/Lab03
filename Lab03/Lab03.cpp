52b1a0cd0af813d13a79df591dafd292a8fef9af
Phyntar <43153541+Phyntar@users.noreply.github.com>
2018-10-01 16:32:30 -0600

    Lab Completed

--- Lab03/Lab03.cpp
+++ Lab03/Lab03.cpp
@@ -5,23 +5,62 @@
 #include <iostream>
 using std::cin;
 using std::cout;
+using std::endl;
 
-short sum(short endVal);
+short sumShort(short endVal);
+long sumLong(long endVal);
+float floatFactorial(long endVal);
+double doubleFactorial(long endVal);
+float sumRecip(float endVal);
 
 int main()
 {
-	while (true)
+	short n = 0;
+	cout << "please enter a short that is for the summationing (n>=1): ";
+	cin >> n;
+
+	cout << "\n\nsum: " << sumShort(n) << "\n\n";
+
+
+	long n2 = 0;
+	cout << "please enter a long that is for the summationing (n>=1): ";
+	cin >> n2;
+
+	cout << "\n\nsum: " << sumLong(n2) << "\n\n";
+
+	long n3 = 0;
+	cout << "please enter a float that is for the facortialing (n>=1): ";
+	cin >> n3;
+
+	cout << "\n\nfactorial: " << floatFactorial(n3) << "\n\n";
+
+	long n4 = 0;
+	cout << "please enter a double that is for the facortialing (n>=1): ";
+	cin >> n4;
+
+	cout << "\n\nfactorial: " << doubleFactorial(n4) << "\n\n";
+
+	
+	float n5 = 0;
+	cout << "please enter a float that is for the sumationing Recipricals (n>=1): ";
+	cin >> n5;
+
+	cout << "\n\nfactorial: " << sumRecip(n5) << "\n\n";
+	
+	for (float i = 3.4; i < 4.4; i += 0.2)
 	{
-		short n = 0;
-		cout << "please enter a number that is for the summationing (n>=1): ";
-		cin >> n;
-
-		cout << "\n\nsum: " << sum(n) << "\n\n";
-
+		cout << "i = " << i << endl;
 	}
-}
+	cout << endl;
+	for (double i = 3.4; i < 4.4; i += 0.2)
+	{
+		cout << "i = " << i << endl;
+	}
+	cin >> n;
+}	
 
-short sum(short endVal)
+
+short sumShort(short endVal)
 {
 	if (endVal>= 1)
 	{
@@ -31,6 +70,66 @@ short sum(short endVal)
 			sum += i;
 		}
 		return sum;
+
+	}
+	return 0;
+}
+
+long sumLong(long endVal)
+{
+	if (endVal >= 1)
+	{
+		long sum = 0;
+		for (long i = 1; i <= endVal; i++)
+		{
+			sum += i;
+		}
+		return sum;
+
+	}
+	return 0;
+}
+
+float floatFactorial(long endVal)
+{
+	if (endVal >= 1)
+	{
+		float sum = 1;
+		for (long i = 1; i <= endVal; i++)
+		{
+			sum *= i;
+		}
+		return sum;
+
+	}
+	return 0;
+}
+
+double doubleFactorial(long endVal)
+{
+	if (endVal >= 1)
+	{
+		double sum = 1;
+		for (long i = 1; i <= endVal; i++)
+		{
+			sum *= i;
+		}
+		return sum;
+
+	}
+	return 0;
+}
+
+float sumRecip(float endVal)
+{
+	if (endVal >= 1)
+	{
+		float sum = 0;
+		for (float i = 1; i <= endVal; i++)
+		{
+			sum += 1/i;
+		}
+		return sum-1;
 
 	}
 	return 0;
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Lab03
+
+# Worked with Jon Cauldwell, Matt McMenamin, James Schuchardt
+2: Overflow Occurs at n=256 overflow can be detected my making sure the sum is never negative
+3: Overflow occurs at n=65,536 overflow can be detected my making sure the sum is never negative
+4: Overflow does not occur it instead starts returning "inf" at 35
+5: Overflow does not occur it instead starts returning "inf" at 171
+6: while 1/n will continue to grow, it grows so slowly the input would become too large to input before the output overflows
+7: the numeric error is that it prints out 4.4 instead of stopping at 4.4 because 4.4 is not < 4.4 because floats save numbers differently
+8: If the float is replaced with a double, it correctly stops at 4.2
